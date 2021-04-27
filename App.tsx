269808b175cbb3137d89bbcda9c2410b5682f81d
Luke Short <lshort2@mail.csuchico.edu>
2021-04-26 19:37:58 -0700

    add react-native-paper debounce serach

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 // import NewComponent from './src/components/NewComponent/NewComponent';
 import SearchComponent from './src/components/SearchComponent/SearchComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>example text</Text>
-      <StatusBar style="auto" />
-      <SearchComponent/>
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>example text</Text>
+        <StatusBar style="auto" />
+        <SearchComponent />
+      </View>
+    </PaperProvider>
   );
 }
 
--- src/components/SearchComponent/SearchComponent.tsx
+++ src/components/SearchComponent/SearchComponent.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from "react-native";
-import { Component } from 'react';
+import { View } from "react-native";
+import { Searchbar } from 'react-native-paper';
+const SEARCH_DEBOUNCE_TIME = 2000;
+
+
+function debounce(func: any, wait: number) { // https://medium.com/@kartikag01/debounce-api-request-in-react-functional-component-664b4971d9dd
+  let timeout: any;
+  return function(...args: any[]) {
+    const context: any = this;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+}
 
 
 const SearchComponent = () => {
-  const [text, onChangeText] = React.useState("Useless Text");
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const debounceOnChange = React.useCallback(debounce(call_api, SEARCH_DEBOUNCE_TIME), []);
+
+  function call_api(search_query: string) {
+    if (search_query) {
+      console.log('send this data to API: ', search_query);
+    }
+  }
+
+
   return (
     <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={
-          (change) => {
-            console.log('change event: ', change);
-            onChangeText(text + '0')
-          }
-        }
-        value={text}
+      <Searchbar
+        placeholder="Search"
+        onChangeText={(search_query) => {
+          setSearchQuery(search_query);
+          debounceOnChange(search_query);
+        }}
+        value={searchQuery}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: 'salmon'
-  },
-});
 export default SearchComponent;
